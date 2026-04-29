c578504bb05574a07598966af5d85bd2338034e9
keroro7729 <keroro06108@gmail.com>
2026-04-29 18:01:18 +0900

    client 템플릿 추가

---
+++ back/app/infra/xxxx_client.py
@@ -0,0 +1,13 @@
+
+
+# 미국 날씨 API를 관리하는 client
+
+# 외부 API 호출, (예외처리 등 안전처리), 결과 파싱을 담당
+
+# 함수형도 가능하고, 클래스 형으로 만들어보셔도 좋습니다.
+# 실제로 내부 서비스에서 사용할때 어떻게 사용하는게 편할까도 고려해보면 좋을거같습니다.
+
+def get_weather(location, current_datetime):
+    # TODO
+    weather_data = {}
+    return weather_data
