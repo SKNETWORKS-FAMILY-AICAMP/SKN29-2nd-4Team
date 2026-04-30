177d18d7d2f686d7a2b8bfb2bd791e646286a9f8
keroro7729 <keroro06108@gmail.com>
2026-04-30 20:47:05 +0900

    코드 테이블 생성 sql, script 추가

--- back/sql/table.sql
+++ back/sql/table.sql
@@ -4,3 +4,28 @@ CREATE TABLE pred_models (
     model JSON NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE airport_info(
+    airport_code CHAR(3) PRIMARY KEY, 
+    airport_name_en VARCHAR(128), 
+    airport_name_ko VARCHAR(128), 
+    taxiout_mean FLOAT, 
+    taxiin_mean FLOAT
+);
+
+CREATE TABLE airtime (
+    route CHAR(7) NOT NULL,
+    crs_dep_hour INT NOT NULL,
+    airtime_mean FLOAT,
+
+    PRIMARY KEY (route, crs_dep_hour)
+);
+
+CREATE TABLE taxi_inout (
+    airport_code CHAR(3) NOT NULL,
+    operation_hour INT NOT NULL,
+    taxiin_mean FLOAT,
+    taxiout_mean FLOAT,
+
+    PRIMARY KEY (airport_code, operation_hour)
+);
