c41facf9273a317ac76ade757dabc8df00bc6acb
keroro7729 <keroro06108@gmail.com>
2026-05-06 15:02:16 +0900

    불필요한 파일 삭제, ignore 추가

--- run_train.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call C:\Users\seokh\miniconda3\Scripts\activate.bat skn2nd
-set KMP_DUPLICATE_LIB_OK=TRUE
-set MKL_THREADING_LAYER=INTEL
-set OMP_NUM_THREADS=4
-set MKL_NUM_THREADS=4
-python C:\prj2\back\app\ml\rev_03\run.py
-pause
